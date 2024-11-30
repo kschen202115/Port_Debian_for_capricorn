d712bb9fcdf59b2cc954e9d6e1318dd9f5118cba
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2024-11-30 16:49:05 +0800

    Update get_kernel_files.sh

--- get_kernel_files.sh
+++ get_kernel_files.sh
@@ -1,5 +1,1 @@
-mkdir ./tmp_mkboot
-rm -rf ./tmp_mkboot/*
-cp ./linux/arch/arm64/boot/dts/qcom/*capricorn*.dtb ./tmp_mkboot/
-cp ./linux/arch/arm64/boot/Image.gz ./tmp_mkboot/
 cp /mnt/chroot/boot/initrd* ./tmp_mkboot/
