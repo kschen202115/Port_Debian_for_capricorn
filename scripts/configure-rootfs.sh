b7b1abfab328e36f3c166d65d384a76fc075d822
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2026-08-02 02:24:34 +0800

    Set executable permissions for local binaries

--- scripts/configure-rootfs.sh
+++ scripts/configure-rootfs.sh
@@ -201,6 +201,10 @@ chmod 755 /etc/hkdm
 chmod 755 /etc/hkdm/config.d
 chmod 644 /etc/hkdm/config.d/power.toml
 
+chmod 755 /usr/local/bin/*
+chmod 755 /usr/local/libexec/*
+chmod 755 /usr/local/sbin/*
+
 # hkdm / buffyboard。判断的是二进制而不是 unit 文件 —— unit 来自 overlay，
 # 必然存在，拿它做条件等于没判断；二进制来自 usr/bin/，才是真会缺的那个
 if [ -x /usr/bin/hkdm ]; then
